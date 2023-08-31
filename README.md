ecdf535581e850f85a1729268e102ce2ab0c5453
Vishva22122121 <118505178+Vishva22122121@users.noreply.github.com>
2023-09-01 02:07:19 +0530

    data ingestion notebook added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Text-Summarizer-Project
+
+
+## Workflows
+1. Update config.yaml file.
+2. Update params.yaml file.
+3. Update entity.
+4. Update the configuration manager in sc/config.
+5. Update the conponents.
+6. Update the pipeline.
+7. Update the main.py file.
+8. Update the app.py file.
---
+++ src/textSummarizer/config/configuration.py
@@ -0,0 +1,40 @@
+from textSummarizer.constants import *
+from textSummarizer.utils.common import read_yaml, create_directories
+from textSummarizer.entity import (DataIngestionConfig)
+
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath=None,
+        params_filepath=None):
+
+        try:
+            if config_filepath is None:
+                config_filepath = CONFIG_FILE_PATH
+            self.config = read_yaml(config_filepath)
+
+            if params_filepath is None:
+                params_filepath = PARAMS_FILE_PATH
+            self.params = read_yaml(params_filepath)
+
+            create_directories([self.config.artifacts_root])
+
+        except Exception as e:
+            print(f"An error occurred during initialization: {str(e)}")
+
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        try:
+            config = self.config.data_ingestion
+
+            create_directories([config.root_dir])
+
+            data_ingestion_config = DataIngestionConfig(
+                root_dir=config.root_dir,
+                source_URL=config.source_URL,
+                local_data_file=config.local_data_file,
+                unzip_dir=config.unzip_dir 
+            )
+
+            return data_ingestion_config
+        except Exception as e:
+            print(f"An error occurred in get_data_ingestion_config: {str(e)}")
